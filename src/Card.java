86ce077587a0e23560bcc09f7ad3ed542b9f44a3
LittenSchool <19stoyanov_j@bexleygs.co.uk>
2024-11-12 17:33:58 +0000

    balls but mopre

---
+++ src/Card.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Card {
+    String name;
+    int hp;
+    ArrayList<String> moves;
+    String type;
+    String rarity;
+    int retreatCost;
+    String weaknesses;
+
+    public Card(int pokedexNumber) {
+
+        this.name = name;
+        this.hp = hp;
+        this.moves = moves;
+        this.type = type;
+        this.rarity = rarity;
+        this.retreatCost = retreatCost;
+        this.weaknesses = weaknesses;
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("bibbidy babbidy boo");
+        File myObj = new File("pokemonInfo.txt");
+        Scanner myScanner = new Scanner(myObj);
+        while (myScanner.hasNextLine()) {
+            String data = myScanner.nextLine();
+            System.out.println(data);
+        }
+
         }
     }
