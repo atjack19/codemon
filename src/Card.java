5c02e27871fb1ece0a72733c79fc1ba3f35b9401
LittenSchool <19stoyanov_j@bexleygs.co.uk>
2024-11-12 18:39:04 +0000

    W4DFGZY

--- src/Card.java
+++ src/Card.java
@@ -1,25 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Card {
+    int pokedexLength;
     String name;
     int hp;
-    ArrayList<String> moves;
     String type;
     String rarity;
     int retreatCost;
     String weaknesses;
+    ArrayList<String> moves = new ArrayList<>();
+    File myObj = new File("src\\pokemonInfo.txt");
+    Scanner myScanner = new Scanner(myObj);
 
-    public Card(int pokedexNumber) {
+    public Card(int pokedexNumber) throws FileNotFoundException {
 
-        this.name = name;
-        this.hp = hp;
-        this.moves = moves;
-        this.type = type;
-        this.rarity = rarity;
-        this.retreatCost = retreatCost;
-        this.weaknesses = weaknesses;
-
+        int lineCounter = 1;
+        while (lineCounter < pokedexNumber) {
+            myScanner.nextLine();
+            lineCounter++;
+        }
+        String desiredLine = myScanner.nextLine();
+        String regex = ",";
+        String[] pokeInfo = desiredLine.split(regex);
+        name = pokeInfo[0];
+        hp = Integer.parseInt(pokeInfo[1]);
+        type = pokeInfo[2];
+        rarity = pokeInfo[3];
+        retreatCost = Integer.parseInt(pokeInfo[4]);
+        weaknesses = pokeInfo[5];
+        int counter = 6;
+        while (pokeInfo.length > counter) {
+            moves.add(pokeInfo[counter]);
+            counter++;
+        }
     }
-
-
+    public int getPokedexLength() {
+        int lineCounter = 0;
+        while (myScanner.hasNextLine()) {
+            lineCounter++;
+            myScanner.nextLine();
+        }
+        return lineCounter;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("bibbidy babbidy booo");
-        File myObj = new File("pokemonInfo.txt");
-        Scanner myScanner = new Scanner(myObj);
-        while (myScanner.hasNextLine()) {
-            String data = myScanner.nextLine();
-            System.out.println(data);
-        }
+        Card name = new Card(2);
+
 
         }
     }
