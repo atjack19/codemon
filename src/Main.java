869da01f9de5c08278e66043540f63db6ee1a43e
LittenSchool <19stoyanov_j@bexleygs.co.uk>
2024-11-16 13:14:17 +0000

    Pokemon Added: Grass, Water and Fire (only basic info)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
