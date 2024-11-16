8cb223f65ced2f288d8ff3680c545a957bb4b2ea
atjack19 <admiraljackbar07@gmail.com>
2024-11-16 00:48:26 +0000

    IDK the numbers but the json stuff works huzzah

--- src/CardJSON.java
+++ src/CardJSON.java
@@ -1,34 +1,57 @@
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class CardJSON {
     String[] cardData;
     String name;
-    int hp;
+    Long hp;
     String type;
     String rarity;
-    int retreatCost;
-    String weaknesses;
-    ArrayList<String> moves = new ArrayList<>();
+    Long retreatCost;
+    String weakness;
+    ArrayList<String> moves;
     String filePath = "src\\pokedex.json";
 
 
     public CardJSON() throws IOException, ParseException {
-        try (FileReader reader = new FileReader(filePath)) {
-            // read the json file
+        JSONParser parser = new JSONParser();
+        JSONArray pokedex = (JSONArray) parser.parse(new FileReader("src/pokedex.json")); //this is one slash bc I was working on macOS its two backslashes on win
+        Random pokeIndex = new Random();
+        int randPokemon = pokeIndex.nextInt(pokedex.toArray().length);
+            JSONObject pokemon = (JSONObject) pokedex.toArray()[randPokemon];
 
+            name = (String) pokemon.get("Name");
+            System.out.println(name);
 
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-            Object firstName = jsonObject.get(1);
-            System.out.println(firstName);
-        }
+            hp = (Long) pokemon.get("HP");
+            System.out.println(hp);
+
+            type = (String) pokemon.get("Type");
+            System.out.println(type);
+
+            rarity = (String) pokemon.get("Rarity");
+            System.out.println(rarity);
+
+            retreatCost = (Long) pokemon.get("Retreat Cost");
+            System.out.println(retreatCost);
+
+            weakness = (String) pokemon.get("Weakness");
+            System.out.println(weakness);
+
+            moves = (ArrayList<String>) pokemon.get("Moves");
+
+            for (Object c : moves)
+            {
+                System.out.println(c+"");
+            }
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        Card name = new Card("4D");
+        //Card name = new Card("4D");
         CardJSON card = new CardJSON();
 
         }
