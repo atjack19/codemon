1eadbf2ddb3ed802293367a164272d3db79befd1
LittenSchool <19stoyanov_j@bexleygs.co.uk>
2024-11-21 13:22:19 +0000

    Dramatic Card Pulling O.o

---
+++ src/Collection.java
@@ -0,0 +1,10 @@
+public class Collection {
+    /*
+     used to manipulate and display the collection of various users
+     need to add things like:
+     - displaying collections
+     - sorting collections
+     - user input to have different collections
+     - method to delete collections easily
+     */
+}
